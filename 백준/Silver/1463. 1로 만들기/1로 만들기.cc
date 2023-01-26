9051a09dadb3b85fbedac739e310d6ce74f96742
duddn2012 <31121731+duddn2012@users.noreply.github.com>
2023-01-26 22:35:12 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,37 @@
+#include<string>
+#include <iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<regex>
+#include<time.h>
+#include<sstream>
+#include<cmath>
+#include<tuple>
+#include<limits.h>
+#define _CRT_SECURE_NO_WARNINGS
+#define FIO cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
+#define MAX 1000010
+typedef long long int ll;
+using namespace std;
+
+static vector<int> dp;
+
+int main() {
+	FIO;
+	int n ,cnt =0, result =1;
+	cin >> n;
+	dp.resize(n + 1);
+	dp[1] = 0;
+
+	for (int i = 2; i <= n; i++) {
+		dp[i] = dp[i - 1] + 1;
+		if (i % 2 == 0) {
+			dp[i] = min(dp[i], dp[i / 2] + 1);
+		}
+		if (i % 3 == 0) {
+			dp[i] = min(dp[i], dp[i / 3] + 1);
+		}
+	}
+	cout << dp[n];
+}
