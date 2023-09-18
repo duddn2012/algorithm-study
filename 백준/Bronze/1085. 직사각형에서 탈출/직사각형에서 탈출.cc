a2c99fe082eb475fdd961891c3a6dcb7c3a99983
duddn2012 <31121731+duddn2012@users.noreply.github.com>
2023-09-18 19:57:02 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.cc
@@ -0,0 +1,50 @@
+#include<string>
+#include <iostream>
+#include<sstream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<regex>
+#include<time.h>
+#include<cmath>
+#include<math.h>
+#include <list>
+#include <stack>
+#define _CRT_SECURE_NO_WARNINGS
+#define FIO cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
+#define MAX 100000001
+typedef unsigned long long int ll;
+using namespace std;
+
+/**
+* [1085] 직사각형에서 탈출
+* 1. 
+* 
+* [생각해볼 문제]
+* 
+* [반례]
+* 1. 
+*/
+
+int result;
+
+
+int main() {
+	FIO; 
+
+	int x,y, w, h;
+	int dl, dr, du, dd;
+
+	cin >> x >> y >> w >> h;
+	
+	dl = x;
+	dr = w-x;
+
+	du = h-y;
+	dd = y;
+
+	cout << min(min(min(dl, dr), du), dd);
+
+	cout << '\n';
+	return 0;
+}
