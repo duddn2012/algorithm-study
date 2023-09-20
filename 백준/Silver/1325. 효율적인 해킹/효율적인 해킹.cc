954d62ce252032271fbd8ac29e7e8fecfd56beb6
duddn2012 <31121731+duddn2012@users.noreply.github.com>
2023-09-20 22:54:20 +0900

    [Silver I] Title: 효율적인 해킹, Time: 2780 ms, Memory: 3232 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
@@ -19,11 +19,16 @@ using namespace std;
 /**
 * [1325] 효율적인 해킹
 * 1. 신뢰 관계를 에지로, 컴퓨터를 노드로 생각  
-* 2. n = 10000, m = 100000 이므로 dfs를 통해 해결
+* 2. n = 10000, m = 100000 이므로 bfs를 통해 해결
 * 3. 연결 요소들을 모두 조사하며 각각 연결 요소내의 종점 노드들의 연결된 노드의 개수와 종점 노드를 저장한다.
 * 4. 크기를 비교하여 가장 큰 노드 출력
 * 
 * [생각해볼 문제]
+* 이 문제의 핵심은 신뢰 관계를 이해하는데 있다.  B를 해킹하면 A 또한 해킹할 수 있는 관계는
+* B가 주인이 되어 A와 A의 하위 컴퓨터들을 모두 해킹할 수 있는 것이다.
+* 그러므로 dfs를 통해 B 컴퓨터에 연결된 모든 하위 컴퓨터들을 탐색하여 해당 컴퓨터의 갯수가 많은 것들을 출력하면 된다.
+* 즉, 노드를 입력 받을 때, graph[A].push_back(B) 가 아닌 graph[B].push_back(A)이여야 한다.
+* 하위 노드를 기준으로 입력 받아서 처리하면 백트래킹으로 모든 노드마다 카운트를 해줘야하는데 이 방법은 시간 초과가 발생했다.
 * 
 * [반례]
 * 1. 
@@ -35,14 +40,22 @@ int connect[MAX];
 int maximum = 0;
 int cnt=0;
 
-void dfs(int x) {
-	cnt++;
+void bfs(int x) {
+	queue<int> q;
+	q.push(x);
 	visited[x] = true;
-	for (int i = 0; i < g[x].size(); i++) {
-		int adj = g[x][i];
-		if (!visited[adj]) dfs(adj);
+	while (!q.empty()) {
+		int curr = q.front();
+		q.pop();
+		for (int i = 0; i < g[curr].size(); i++) {
+			int adj = g[curr][i];
+			if (visited[adj] == false) {
+				q.push(adj);
+				connect[x]++;
+				visited[adj] = true;
+			}
+		}
 	}
-	maximum = max(maximum, cnt);
 }
 
 int main() {
@@ -61,10 +74,12 @@ int main() {
 	}
 
 	for (int i = 1; i <= n; i++) {
-		dfs(i);
-		connect[i] = cnt;
-		cnt = 0;
+		bfs(i);
 		fill_n(visited, n + 1, 0);
+	}
+	
+	for (int i = 1; i <= n; i++) {
+		if (connect[i] > maximum) maximum = connect[i];
 	}
 
 	for (int i = 1; i <= n; i++) {
