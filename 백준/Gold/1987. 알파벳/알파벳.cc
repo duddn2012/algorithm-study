abe80259a98150757bc4426594287eb6e3a21cc2
고영우 <31121731+duddn2012@users.noreply.github.com>
2023-11-03 14:37:57 +0900

    [Gold IV] Title: 알파벳, Time: 748 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<set>
+#include<queue>
+#include<string>
+#include<climits>
+using namespace std;
+#define FIO cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
+
+vector<vector<char>> v;
+vector<vector<int>> squreVistied;
+vector<int> visited;
+
+int r, c;
+int result=0;
+int dx[] = { 0,1,0,-1 };
+int dy[] = { 1,0,-1,0 };
+
+void dfs(int ir, int ic, int cnt) {
+
+    int alphaPoint = v[ir][ic] - 65;
+
+    if (visited[alphaPoint]) return;
+
+    for (int i = 0; i < 4; i++) {
+        int nx = ir + dx[i];
+        int ny = ic + dy[i];
+
+        if (nx >= 0 && ny >= 0 && nx < r && ny < c) {
+            visited[alphaPoint]++;
+            result = max(result, cnt+1);
+            dfs(nx, ny, cnt+1);
+            visited[alphaPoint]--;
+        }
+    }
+}
+
+int main() {
+    FIO;
+
+    cin >> r >> c;
+    v.resize(r + 1, vector<char>(c + 1));
+    squreVistied.resize(r + 1, vector<int>(c + 1));
+    visited.resize(27, 0);
+
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            cin >> v[i][j];
+        }
+    }
+
+    dfs(0, 0, 0);
+
+    cout << result;
+
+    return 0;
+}
