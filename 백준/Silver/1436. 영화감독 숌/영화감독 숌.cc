0577f22a9be0c39c7e7f0bc3d3f37f02897ff19e
고영우 <31121731+duddn2012@users.noreply.github.com>
2023-12-27 20:22:05 +0900

    [Silver V] Title: 영화감독 숌, Time: 64 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -6,28 +6,20 @@
 #include <algorithm>
 using namespace std;
 
-bool containVal(string str) {
-	for (int i = 0; i < str.size()-2; i++) {
-		if (str[i] == '6' && str[i + 1] == '6' && str[i + 2] == '6') return true;
-	}
-	return false;
-}
-
 int main() {
 	
-	int n, value = 100, cnt = 1;
+	int n, value = 100;
 	cin >> n;
 
 	for(int i=0; i<n;i++){
 		while (true) {
 			value++;
 			string str = to_string(value);
-			if (containVal(str)) {
+			if (str.find("666") != -1) {
 				break;
 			}
 		}
 	}
-
 	cout << value;
 
 	return 0;
