391e4bf5c7c8def1c4e2f6e24700cb24b6a5ea43
고영우 <31121731+duddn2012@users.noreply.github.com>
2024-03-08 16:15:00 +0900

    [Unrated] Title: 타노스, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Unrated/15802. 타노스/타노스.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include<string>
+#include <queue>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int main () {
+    cout << 0;
+    return 0;
+}
