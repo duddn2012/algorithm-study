7c61e12d0007acabbe5ea7918004466fc8aed4c7
고영우 <31121731+duddn2012@users.noreply.github.com>
2024-03-08 15:16:25 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 12 ms, Memory: 2816 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -1,45 +1,39 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<set>
-#include<queue>
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include<string>
+#include <queue>
+#include <algorithm>
+#include <climits>
 using namespace std;
-#define FIO cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
 
-int n, m;
-vector<bool> visited;
+vector<bool> v;
+int n, k;
 
-void bfs() {
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
-    q.push({ 0,n });
-    visited[n] = true;
-    while (!q.empty()) {
-        int time = q.top().first;
-        int x = q.top().second;
-        q.pop();
+int bfs() {
+	priority_queue<pair<int,int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
+	q.push({ 0, n});
+	
+	while (!q.empty()) {
+		int curCnt = q.top().first;
+		int cur = q.top().second;
+		v[cur] = curCnt;
 
-        visited[x] = 1;
+		q.pop();
 
-        if (x == m) {
-            cout << time;
-            break;
-        }
+		v[cur] = true;
 
-        if (x - 1 >= 0 && !visited[x - 1]) q.push(make_pair(time + 1, x - 1));
-        if (x + 1 <= 100000 && !visited[x + 1]) q.push(make_pair(time + 1, x + 1));
-        if (x * 2 <= 100000 && !visited[x * 2]) q.push(make_pair(time, x *2));
+		if (cur == k) return curCnt;
 
-    }
+		if (cur * 2 <= 100000 && !v[cur * 2]) q.push({ curCnt,cur * 2 });
+		if (cur - 1 >= 0 && !v[cur - 1]) q.push({ curCnt + 1, cur - 1 });
+		if (cur + 1 <= 100000 && !v[cur + 1]) q.push({ curCnt + 1, cur + 1 });
+	}
 }
 
-int main() {
-    FIO;
-
-    cin >> n >> m;
-
-    visited.resize(100000);
-
-    bfs();
-
-    return 0;
+int main() {	
+	cin >> n >> k;
+	v.resize(100000);
+	cout << bfs();
+	return 0;
 }
