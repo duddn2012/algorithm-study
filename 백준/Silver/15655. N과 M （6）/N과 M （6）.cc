eba590a8773bd51a2803522b8c133b824ce1748b
고영우 <31121731+duddn2012@users.noreply.github.com>
2024-03-25 01:33:55 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+#define MAX 9
+using namespace std;
+
+int n, m;
+int arr[MAX] = { 0, };
+bool visited[MAX] = { 0, };
+
+vector<int> input;
+
+void dfs(int num, int cnt)
+{
+    if (cnt == m)
+    {
+        for (int i = 0; i < m; i++)
+            cout << arr[i] << ' ';
+        cout << '\n';
+        return;
+    }
+    for (int i = num; i < n; i++)
+    {
+        if (!visited[i])
+        {
+            visited[i] = true;
+            arr[cnt] = input[i];
+            dfs(i + 1, cnt + 1);
+            visited[i] = false;
+        }
+    }
+}
+
+int main() {
+    int temp;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> temp;
+        input.push_back(temp);
+    }
+    sort(input.begin(), input.end());
+    dfs(0, 0);
+}
+
+
