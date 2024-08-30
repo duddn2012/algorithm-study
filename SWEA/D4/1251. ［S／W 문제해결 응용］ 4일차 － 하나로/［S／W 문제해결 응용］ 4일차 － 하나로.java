6f4b3d0498ed4e34b8c919a2dff53d2638c1f11f
고영우 <31121731+duddn2012@users.noreply.github.com>
2024-08-30 15:25:00 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 3,561 ms, Memory: 152,208 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 
@@ -16,7 +17,11 @@ import java.util.StringTokenizer;
  * 
  * 모든 섬들을 연결해야함.
  * 섬을 연결할 때 최소 비용으로 그리디하게 연결
- * 크루스칼
+ * 프림
+ * 
+ * 1. 정점 하나를 선정
+ * 2. 정점과 연결된 간선들 중 비용이 가장 작은 정점을 찾아 트리에 추가
+ * 3.  
  */
 public class Solution {
 	
@@ -25,19 +30,16 @@ public class Solution {
 	static StringTokenizer st2;
 	static StringBuilder sb = new StringBuilder();			
 	
-	static boolean visited[];
-	
-	static int[] parents;
+	static boolean visited[];	
 	
 	static int islandCount;	
 	static Point[] islands;
-	
-	static List<Edge> edges;
+
+	static int edgeCount;
+	static List<Edge>[] edges;
 	
 	static double faxRate;
-	
-	static int edgeCount; 
-	
+		
 	static double result;	
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -47,9 +49,9 @@ public class Solution {
 		for(int test_case = 1; test_case <= T; test_case++) {
 			islandCount = Integer.parseInt(br.readLine().trim());												
 			
+			visited = new boolean[islandCount+1];
 			islands = new Point[islandCount+1];
-			parents = new int[islandCount+1];
-			edges = new LinkedList<>();										
+			edges = new LinkedList[islandCount + 1];										
 			edgeCount = 0;
 			result =0;
 			
@@ -60,6 +62,7 @@ public class Solution {
 				int row = Integer.parseInt(st.nextToken());
 				int col = Integer.parseInt(st2.nextToken());
 				
+				edges[index] = new LinkedList<>();
 				islands[index] = new Point(row, col);
 			}
 		
@@ -72,49 +75,49 @@ public class Solution {
 					Point fromIsland = islands[from];
 					Point toIsland  = islands[to];
 					double cost = (Math.pow(fromIsland.row - toIsland.row, 2) + Math.pow(fromIsland.col - toIsland.col, 2)) * faxRate;					
-					edges.add(new Edge(from, to, cost));
+					edges[from].add(new Edge(from, to, cost));
+					edges[to].add(new Edge(to, from,cost));					
 				}
 			}
-			
-			// 최소 단위 서로소 집합 생성
-			for(int index=1; index<= islandCount; index++) {
-				parents[index] = -1;
-			}
-			
-			// 간선 정렬
-			edges.sort(Edge::compareTo);
 			
 			solve();
 					
 			sb.append("#" + test_case+ " "+Math.round(result)+ "\n");									
-		}	
+		}
 		System.out.println(sb);
 	}
 		
+	// 1. 정점에 연결된 간선을 pq에 추가한다.
+	// 2. pq에 저장된 가장 비용이 낮은 간선과 연결한다.
+	// 1 ~ 2 반복
 	static void solve() {
-		for (Edge edge : edges) {
-			if(union(edge.from, edge.to)) {
-				result += edge.cost;
-				if(++edgeCount == islandCount -1) break;
+		PriorityQueue<Edge> pq = new PriorityQueue<>((edge1, edge2)-> {
+			if(edge1.cost >= edge2.cost) return 1;
+			else return -1;
+		});
+		
+		// 초기 정점 정보 입력
+		pq.add(new Edge(1, 1, 0));
+		
+		while(!pq.isEmpty()) {
+			Edge curEdge = pq.remove();
+			
+			// 이미 트리 정점에 포함되어 있다면 패스
+			if(visited[curEdge.to]) continue;
+			
+			// 아니라면 트리 정점에 포함하고 pq에 연결된 간선들 추가
+			visited[curEdge.to]= true;
+			result += curEdge.cost;
+			
+			for(int edgeIndex=0; edgeIndex < edges[curEdge.to].size(); edgeIndex++) {
+				pq.add(edges[curEdge.to].get(edgeIndex));				
 			}
 		}
 	}	
-	
-	private static int find(int node) {
-		if(parents[node] == -1) return node;
-		else return parents[node] = find(parents[node]);
-	}
-
-	// 2. union: 각각의 최상위 부모 노드를 찾아서 같을 경우 수행 x, 다를 경우 왼쪽 노드로 부모 통일. 그 후, 재귀적으로 부모노드 갱신 처리
-	private static boolean union(int left, int right) {
-		if(find(left) == find(right)) return false;
-		else parents[find(right)] = find(left);
-		return true;
-	}
-
+		
 	static class Edge implements Comparable<Edge>{
 		int from;
-		int to;		
+		int to;
 		double cost;
 		
 		public Edge(int from, int to, double cost) {
