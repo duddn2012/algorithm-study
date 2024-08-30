746e087682e167a2d3a6ea62c874613110b2e9a0
고영우 <31121731+duddn2012@users.noreply.github.com>
2024-08-30 15:43:30 +0900

    [D4] Title: 최소 스패닝 트리, Time: 4,875 ms, Memory: 287,856 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 
@@ -14,97 +15,99 @@ import java.util.StringTokenizer;
  * SWEA 3124 최소 스패닝 트리
  * @author SSAFY
  * 
- * 모든 섬들을 연결해야함.
- * 섬을 연결할 때 최소 비용으로 그리디하게 연결
- * 크루스칼
+ * 프림
+ * 
+ * 1. 정점 하나를 선정
+ * 2. 정점과 연결된 간선들 중 비용이 가장 작은 정점을 찾아 트리에 추가
+ * 3. 1~2 반복
  */
 public class Solution {
 	
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
+	static StringTokenizer st2;
 	static StringBuilder sb = new StringBuilder();			
 	
-	static boolean visited[];
+	static boolean visited[];	
 	
-	static Edge[] edges;
-	static int[] parents;
-	
-	static int vertaxCount;
-	static int totalEdgeCount;
-	
-	static int curEdgeCount;
-	
-	static long result;	
+	static int vertexCount;	
+
+	static int edgeCount;
+	static List<Edge>[] edges;
+			
+	static double result;	
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		int T = Integer.parseInt(br.readLine().trim());
 		
 		// 입력 부분
-		for(int test_case = 1; test_case <= T; test_case++) {																				
-													
-			totalEdgeCount = 0;
-			curEdgeCount = 0;
-			result =0;
-			
+		for(int test_case = 1; test_case <= T; test_case++) {
 			st = new StringTokenizer(br.readLine().trim(), " ");
 			
-			vertaxCount = Integer.parseInt(st.nextToken());
-			totalEdgeCount = Integer.parseInt(st.nextToken());
+			vertexCount = Integer.parseInt(st.nextToken());												
+			edgeCount = Integer.parseInt(st.nextToken());
 			
-			edges  = new Edge[totalEdgeCount];
-			parents = new int[vertaxCount+1];
+			visited = new boolean[vertexCount+1];
+			edges = new LinkedList[vertexCount + 1];
+			result =0;
 			
-			// 간선 데이터 생성, 생성 시 비용을 계산해서 넣어주자
-			for(int index=0; index < totalEdgeCount; index++) {
-				st = new StringTokenizer(br.readLine().trim()," ");
+			// 정점 별 간선 리스트 초기화
+			for(int index=1; index <= vertexCount; index++) {
+				edges[index] = new LinkedList<>();
+			}
+			
+			for(int index=1; index <= edgeCount; index++) {
+				st = new StringTokenizer(br.readLine().trim(), " ");
 				int from = Integer.parseInt(st.nextToken());
 				int to = Integer.parseInt(st.nextToken());
 				int cost = Integer.parseInt(st.nextToken());
-				edges[index] = new Edge(from, to, cost);			
-			}
-			
-			// 최소 단위 서로소 집합 생성
-			for(int index=1; index<= vertaxCount; index++) {
-				parents[index] = -1;
-			}
-			
-			// 간선 정렬
-			Arrays.sort(edges);
+				
+				// 양방향 간선 생성
+				edges[from].add(new Edge(from, to, cost));
+				edges[to].add(new Edge(to, from, cost));
+			}		
 			
 			solve();
 					
-			sb.append("#" + test_case+ " "+result+ "\n");									
-		}	
+			sb.append("#" + test_case+ " "+Math.round(result)+ "\n");									
+		}
 		System.out.println(sb);
 	}
 		
+	// 1. 정점에 연결된 간선을 pq에 추가한다.
+	// 2. pq에 저장된 가장 비용이 낮은 간선과 연결한다.
+	// 1 ~ 2 반복
 	static void solve() {
-		for (Edge edge : edges) {
-			if(union(edge.from, edge.to)) {
-				result += edge.cost;
-				if(++curEdgeCount == vertaxCount -1) break;
+		PriorityQueue<Edge> pq = new PriorityQueue<>((edge1, edge2)-> {
+			if(edge1.cost >= edge2.cost) return 1;
+			else return -1;
+		});
+		
+		// 초기 정점 정보 입력
+		pq.add(new Edge(1, 1, 0));
+		
+		while(!pq.isEmpty()) {
+			Edge curEdge = pq.remove();
+			
+			// 이미 트리 정점에 포함되어 있다면 패스
+			if(visited[curEdge.to]) continue;
+			
+			// 아니라면 트리 정점에 포함하고 pq에 연결된 간선들 추가
+			visited[curEdge.to]= true;
+			result += curEdge.cost;
+			
+			for(int edgeIndex=0; edgeIndex < edges[curEdge.to].size(); edgeIndex++) {
+				pq.add(edges[curEdge.to].get(edgeIndex));				
 			}
 		}
-	}	
-	
-	private static int find(int node) {
-		if(parents[node] == -1) return node;
-		else return parents[node] = find(parents[node]);
 	}
-
-	// 2. union: 각각의 최상위 부모 노드를 찾아서 같을 경우 수행 x, 다를 경우 왼쪽 노드로 부모 통일. 그 후, 재귀적으로 부모노드 갱신 처리
-	private static boolean union(int left, int right) {
-		if(find(left) == find(right)) return false;
-		else parents[find(right)] = find(left);
-		return true;
-	}
-
+		
 	static class Edge implements Comparable<Edge>{
 		int from;
-		int to;		
-		int cost;
+		int to;
+		double cost;
 		
-		public Edge(int from, int to, int cost) {
+		public Edge(int from, int to, double cost) {
 			super();
 			this.from = from;
 			this.to = to;
@@ -113,20 +116,9 @@ public class Solution {
 
 		@Override
 		public int compareTo(Edge o) {
-			return Integer.compare(this.cost, o.cost);
+			return Double.compare(this.cost, o.cost);
 		}				
-	}
-	
-	static class Point{
-		int row;
-		int col;
-		
-		public Point(int row, int col) {
-			super();
-			this.row = row;
-			this.col = col;
-		}				
-	}		
+	}	
 }
 
 
