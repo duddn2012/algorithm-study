50464a3badd96aef4b6d0f1608fb8489ea985466
고영우 <31121731+duddn2012@users.noreply.github.com>
2025-12-21 15:16:35 +0900

    [Silver I] Title: 지름길, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.java
+++ 백준/Silver/1446. 지름길/지름길.java
@@ -7,18 +7,6 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 /**
- * 그리디 최소값 갱신 반복
- * 
- *
-8 900
-0 10 9
-20 60 45    // 19 + 45 = 64  / 19 + 40
-80 190 100  // 79 + 100
-50 70 15    
-160 180 14
-140 160 14
-420 901 5
-450 900 0
  */
 class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,15 +15,17 @@ class Main {
 
     static int n, d; 
     static List<int[]> list = new ArrayList<>();
-    static int[] status = new int[10001];
+    static int[] dp;
 
     public static void main(String[] args) throws IOException {
         st = new StringTokenizer(br.readLine(), " ");
         n = Integer.parseInt(st.nextToken());
         d = Integer.parseInt(st.nextToken());
 
-        for(int i=1; i<=d;i++){
-            status[i] = 99999;
+        dp = new int[d+1];
+
+        for(int i=0; i<=d;i++){
+            dp[i] = i;
         }
 
         for(int i=0; i<n; i++) {
@@ -45,30 +35,35 @@ class Main {
             int end = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
 
+            if(end -start <= cost || end > d) continue;
+
             list.add(new int[]{start, end, cost});
         }
 
         list.sort((a,b)->a[0]-b[0]);
 
-        for(int[] shortCut: list) {
-            int start = shortCut[0], end = shortCut[1], cost=shortCut[2];
+        for(int[] shortcut: list) {
 
-            int startMinCost = getMinCost(start);
+            int start = shortcut[0];
+            int end = shortcut[1];
+            int cost = shortcut[2];
 
-            if(end - start <= cost) continue;
-
-            status[end] = Math.min(status[end], startMinCost + cost);
+            solve(start, end, cost);
         }
 
-        System.out.println(getMinCost(d));
+        System.out.println(dp[d]);
     }
 
-    static int getMinCost(int value) {
-        int minCost = 99999;
-        for(int i=0; i<=value; i++) {
-            minCost = Math.min(minCost, (status[i] == 99999 ? i : status[i]) + value - i);
+    static void solve(int start, int end, int cost) {
+        if(dp[end] > dp[start] + cost) {
+            dp[end] = dp[start] + cost;
+            updateDp(end);
         }
+    }
 
-        return minCost;
+    static void updateDp(int value) {
+        for(int i=value; i<=d; i++) {
+            dp[i] = Math.min(dp[i], dp[value] + i - value);
+        }
     }
 }
